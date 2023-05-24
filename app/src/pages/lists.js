d7a98b58974ad8921b8b8eff04d1a2da3777f1ad
castanheta <castanheta@ua.pt>
2023-05-24 20:00:26 +0100

    Improved list making

--- app/src/pages/lists.js
+++ app/src/pages/lists.js
@@ -1,124 +1,113 @@
-import React, {useState} from 'react';
-import { KeyboardAvoidingView, StyleSheet, Text, View, TextInput, TouchableOpacity, Keyboard, ScrollView } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, FlatList } from 'react-native';
 import { FontAwesome } from 'react-native-vector-icons';
-import Task from './Task';
 
-function ListsScreen() {
-  const [task, setTask] = useState();
-  const [taskItems, setTaskItems] = useState([]);
 
-  const handleAddTask = () => {
-    Keyboard.dismiss();
-    setTaskItems([...taskItems, task])
-    setTask(null);
-  }
+function Notebook() {
+  const [notes, setNotes] = useState([]);
+  const [currentNote, setCurrentNote] = useState('');
 
-  const completeTask = (index) => {
-    let itemsCopy = [...taskItems];
-    itemsCopy.splice(index, 1);
-    setTaskItems(itemsCopy)
-  }
+  const handleAddNote = () => {
+    if (currentNote.trim() !== '') {
+      setNotes([...notes, currentNote]);
+      setCurrentNote('');
+    }
+  };
+
+  const handleDeleteNote = (index) => {
+    const updatedNotes = [...notes];
+    updatedNotes.splice(index, 1);
+    setNotes(updatedNotes);
+  };
+
+  const handleEditNote = (index, newText) => {
+    const updatedNotes = [...notes];
+    updatedNotes[index] = newText;
+    setNotes(updatedNotes);
+  };
 
   return (
     <View style={styles.container}>
-      {/* Added this scroll view to enable scrolling when list gets longer than the page */}
-      <ScrollView
-        contentContainerStyle={{
-          flexGrow: 1
-        }}
-        keyboardShouldPersistTaps='handled'
-      >
-
-      {/* Today's Tasks */}
       <View style={styles.tasksWrapper}>
         <View style={styles.header}>
-          <Text style={styles.sectionTitle}>Shopping List</Text>
-          <FontAwesome name='shopping-cart' size={24} color='black' marginTop={5}/>
-        </View>
-        <View style={styles.items}>
-          {/* This is where the tasks will go! */}
-          {
-            taskItems.map((item, index) => {
-              return (
-                <TouchableOpacity key={index}  onPress={() => completeTask(index)}>
-                  <Task text={item} /> 
-                </TouchableOpacity>
-              )
-            })
-          }
+            <Text style={styles.sectionTitle}>Shopping List</Text>
+            <FontAwesome name='shopping-cart' size={24} color="#e5e1d8" marginTop={5}/>
         </View>
       </View>
-        
-      </ScrollView>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          multiline
+          placeholder="Enter a note"
+          value={currentNote}
+          onChangeText={(text) => setCurrentNote(text)}
+        />
+        <Button title="Add Note" onPress={handleAddNote} />
+      </View>
 
-      {/* Write a task */}
-      {/* Uses a keyboard avoiding view which ensures the keyboard does not cover the items on screen */}
-      <KeyboardAvoidingView 
-        behavior={Platform.OS === "ios" ? "padding" : "height"}
-        style={styles.writeTaskWrapper}
-      >
-        <TextInput style={styles.input} placeholder={'Write a task'} value={task} onChangeText={text => setTask(text)} />
-        <TouchableOpacity onPress={() => handleAddTask()}>
-          <View style={styles.addWrapper}>
-            <Text style={styles.addText}>+</Text>
+      <FlatList
+        data={notes}
+        renderItem={({ item, index }) => (
+          <View style={styles.noteContainer}>
+            <TextInput
+              style={styles.noteInput}
+              value={item}
+              onChangeText={(text) => handleEditNote(index, text)}
+            />
+            <Button title="Delete" onPress={() => handleDeleteNote(index)} />
           </View>
-        </TouchableOpacity>
-      </KeyboardAvoidingView>
-      
+        )}
+        keyExtractor={(item, index) => index.toString()}
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+const styles = {
   container: {
     flex: 1,
-    marginBottom:50,
-
+    padding: 16,
+    backgroundColor: "#333",
   },
-  tasksWrapper: {
-    paddingTop: 80,
-    paddingHorizontal: 20,
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 16,
+  },
+  inputContainer: {
+    marginBottom: 16,
+  },
+  input: {
+    borderWidth: 1,
+    backgroundColor: '#b4d7dd',
+    padding: 8,
+  },
+  noteContainer: {
+    flexDirection: 'row',
+    marginBottom: 8,
+  },
+  noteInput: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: 'gray',
+    padding: 8,
+    backgroundColor: '#b4d7dd',
+  },
+  header: {
+    flexDirection: 'row',
   },
   sectionTitle: {
     fontSize: 24,
     fontWeight: 'bold',
     flexDirection: 'row',
     marginRight: 10,
+    color: "#e5e1d8",
   },
-  items: {
-    marginTop: 30,
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+    paddingBottom: 40,
   },
-  writeTaskWrapper: {
-    position: 'absolute',
-    bottom: 60,
-    width: '100%',
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    alignItems: 'center'
-  },
-  input: {
-    paddingVertical: 15,
-    paddingHorizontal: 15,
-    backgroundColor: '#FFF',
-    borderRadius: 60,
-    borderColor: '#C0C0C0',
-    borderWidth: 1,
-    width: 250,
-  },
-  addWrapper: {
-    width: 60,
-    height: 60,
-    backgroundColor: '#FFF',
-    borderRadius: 60,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderColor: '#C0C0C0',
-    borderWidth: 1,
-  },
-  addText: {},
-  header: {
-    flexDirection: 'row',
-  }
-});
+};
 
-export default ListsScreen;
+export default Notebook;
